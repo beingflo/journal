850b841cd9361b2c316853836f19fb0cc0653469
Florian Marending <florian@marending.dev>
2024-04-07 13:50:25 +0200

    Add favicon and adapt help screen

--- src/Help.tsx
+++ src/Help.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'solid-js';
+import Logo from '../src/bulb.svg';
 
 const Help: Component = () => {
   const Instruction = props => {
@@ -13,10 +14,10 @@ const Help: Component = () => {
   return (
     <div class="max-w-2xl mt-4 md:mt-12 mx-auto px-4">
       <div class="flex flex-row gap-4 items-center">
-        // Icon
-        <h1 class="text-2xl font-bold tracking-tight">rest.quest</h1>
+        <Logo class="w-12 h-12" />
+        <h1 class="text-2xl font-bold tracking-tight">jour</h1>
       </div>
-      <p class="mt-4">A tiny, opinionated todo list application.</p>
+      <p class="mt-4">A tiny, opinionated lab journal application.</p>
       <p class="md:hidden mt-4 text-red-600">
         Best enjoyed on a larger screen and with keyboard input, sorry!
       </p>
@@ -25,22 +26,17 @@ const Help: Component = () => {
       </p>
       <Instruction left="h" right="Show help screen" />
       <Instruction left="f" right="Show feedback screen" />
-      <Instruction left="p" right="New project" />
-      <Instruction left="n" right="New quest in the current project" />
-      <Instruction left="click on quest" right="Check off quest" />
       <Instruction left="c" right="Show configuration screen" />
+      <Instruction left="n" right="New journal entry" />
       <Instruction left="s" right="Synchronize state with remote if configured" />
-      <Instruction left="v" right="Cycle views of quests" />
-      <Instruction left="arrow up" right="Select previous project" />
-      <Instruction left="arrow down" right="Select next project" />
-      <Instruction left="ctrl+e" right="Compact selected project" />
-      <Instruction left="b y e" right="Purge all local data" />
+      <Instruction left="arrow up" right="Select previous auto tag" />
+      <Instruction left="arrow down" right="Select next auto tag" />
 
       <h2 class="text-xl font-semibold mt-12">S3 synchronization and backup</h2>
       <p class="mt-4 pb-10">
         In the configuration of this app, you can add an endpoint and credentials for an
         S3 provider. If this is provided, the application will synchronize the local state
-        with the S3 bucket after creating new quests, or pressing <b>s</b>.
+        with the S3 bucket after creating new entries, or pressing <b>s</b>.
       </p>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import solidSvg from 'vite-plugin-solid-svg';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), solidSvg()],
   build: {
-    target: 'esnext'
-  }
-})
+    target: 'esnext',
+  },
+});
