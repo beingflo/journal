a3e5fa09f94b4544cc49e0ea5fa130b8e1e0f6fa
Florian Marending <florian@marending.dev>
2025-04-28 22:37:26 +0200

    [infra] Switch to deployment script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+die() { echo "$*" 1>&2 ; exit 1; }
+
+echo -e "Deploying jour.rest.quest to production!"
+
+[ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
+
+npm run eslint || die "ESLint failed"
+npm run build || die "Build failed"
+
+scp -r dist/* omni:./caddy/site/jour.rest.quest/ || die "Failed to copy files to server"
